7dc0974d49dbc7a4410b1b191cfd9e8182cc453f
Jacob Resman <jare4671@colorado.edu>
2014-09-10 11:46:09 -0600

    modified file

--- test.c
+++ test.c
@@ -2,14 +2,15 @@ int addOrSub(int x, int y){
 
   if(x < y) return x + y;
   else return x-y;
-
+//This is a test commnet
 }
 
 int main(){
   
   int x = 5;
   int y = 7;
-
+  
+  printf("Hello World");
   addOrSub(x,y);
   return 0;
 }
